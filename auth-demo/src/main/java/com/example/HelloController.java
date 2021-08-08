dd42d6152087d0bc094c6728d568bb8c930b1f22
bsfeng <bsfeng@foxmail.com>
2021-08-08 21:57:01 +0800

    test: 添加静态类使用测试

--- auth-demo/src/main/java/com/example/HelloController.java
+++ auth-demo/src/main/java/com/example/HelloController.java
@@ -1,33 +1,29 @@
 package com.example;
 
+import com.yizhu.auth.TokenManager;
 import com.yizhu.auth.utils.TokenUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	Logger log = LoggerFactory.getLogger(HelloController.class);
-	@Autowired
-	private TokenUtils tokenUtils;
-
 	@GetMapping("/login")
 	public String login(Long id) {
+		System.out.println(TokenManager.getConfig());
+		System.out.println(TokenManager.getTokenDao());
 		User user = new User(id);
-		return tokenUtils.login(user);
+		return TokenUtils.login(user);
 	}
 
 	@GetMapping("/info")
 	public Long getUserId() {
-		return tokenUtils.getId();
+		return TokenUtils.getId();
 	}
 
 	@GetMapping("/logout")
 	public String logout() {
-		tokenUtils.logout();
+		TokenUtils.logout();
 		return "退出成功";
 	}
 }
