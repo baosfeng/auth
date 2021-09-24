331ba9baa37412f06ea4fda8ee620d2b178d31ca
bsfeng <bsfeng@foxmail.com>
2021-09-24 17:58:30 +0800

    fix: 修复注解运行时不生效的问题

--- src/main/java/xyz/bsfeng/auth/anno/IgnoreLogin.java
+++ src/main/java/xyz/bsfeng/auth/anno/IgnoreLogin.java
@@ -1,10 +1,15 @@
 package xyz.bsfeng.auth.anno;
 
+import java.lang.annotation.*;
+
 /**
  * 当前接口可不登录进行访问
  *
  * @author bsfeng
  * @date 2021/9/22 14:34
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface IgnoreLogin {
 }
